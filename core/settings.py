8d80078073b2e52617e2be11e9f602de9fe2ca07
Dawood <sulaimondawod@gmail.com>
2024-01-01 16:24:33 +0100

    added keys

--- core/settings.py
+++ core/settings.py
@@ -42,7 +42,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('SECURED_SERVER_URL').split(' ')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
