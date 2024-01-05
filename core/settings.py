fbd2db83f8c24356f4bf831433d9adf96e03fbd7
Dawood <sulaimondawod@gmail.com>
2024-01-05 09:36:08 +0100

    latest commit

--- core/settings.py
+++ core/settings.py
@@ -179,10 +179,7 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-#    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=2),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    # "REFRESH_TOKEN_LIFETIME": timedelta(seconds=10),
-
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
